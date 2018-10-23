042b0ed0df965b78c3e991b8fac3b6b72cf5d351
Diogo Ide <diogoide@gmail.com>
2018-10-23 00:20:11 +0000

    automatically backed up by learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,4 +1,4 @@
 class ChangeDatatypeForBirthday < ActiveReord::Migration[5.1]
-  def change_column(students, birthdate, datetime).
-  end 
-end 
+  def change_column(students, birthdate, datetime)
+  end
+end
